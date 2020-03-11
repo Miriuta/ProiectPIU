f78e1858dbae6dd37234f3def72b439afb8886e7
Miriuta <62062617+Miriuta@users.noreply.github.com>
2020-03-11 15:12:11 +0200

    Add files via upload

---
+++ Masina.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect
+{
+    //clasa Masina
+    public class Masina
+    {
+        string marca;
+        string culoare;
+        double pret;
+        int anFabricatie;
+
+
+        //Constructor fara parametrii
+        public Masina()
+        {
+            marca = string.Empty;
+            culoare = string.Empty;
+            pret = 0;
+            anFabricatie = 0;
+
+        }
+        //Constructor cu parametrii
+        public Masina(string _marca, string _culoare, double _pret, int _anFabricatie)
+        {
+            marca = _marca;
+            culoare = _culoare;
+            pret = _pret;
+            anFabricatie = _anFabricatie;
+
+        }
+        public string Info()
+        {
+            return string.Format("Masina este {0}, are culoarea {1}, pretul de {2}, din anul {3}", marca, culoare, pret, anFabricatie);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //	Instantierea unui obiect de tip Masina utilizand constructorul fara parametri
+            
+            var m = new Masina();
+            string s = m.Info();
+            Console.WriteLine(s);
+
+            //	Instantierea unui obiect de tip Masina utilizand constructorul cu parametri
+           
+            Masina m1 = new Masina("VW", "negru", 4.500, 2006);
+            string s1 = m1.Info();
+            Console.WriteLine(s1);
+            Console.ReadKey();
+
+
+        }
+    }
+}
